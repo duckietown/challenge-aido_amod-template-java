b435ba49c57a0783dce5b0fe777cc73e26ac0621
clruch <claudio.ruch@gmail.com>
2018-10-26 14:52:48 +0200

    wip, 339

--- solution.py
+++ solution.py
@@ -21,7 +21,11 @@ class Solver(ChallengeSolution):
 	cis.info(os.getcwd())
 	dirlist = os.listdir(os.getcwd())
 	cis.info(dirlist)
-        cmd = ['java', '-jar', 'aidamod-1.4.7.jar', 'aido-host']
+	cis.info('showing java version:')
+	cmdversion = ['java','-version']
+        subprocess.check_call(cmdversion, cwd=cwd, stdout=sys.stdout, stderr=sys.stderr)
+#        cmd = ['java', '-jar', 'aidamod-1.4.7.jar', 'aido-host']
+        cmd = ['java', '-cp', 'aidamod-1.4.7.jar','aidamod.demo.AidoGuest', 'aido-host']
 #               '-cp', cp,
 #               'aidamod.demo.AidoGuest', 'aido-host']
         cis.info('Now running %s' % cmd)
